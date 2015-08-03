5fd2755deafb58743a203bca0d478e006dcfdc40
administrador <admin@admin.com>
2015-08-03 17:04:06 -0300

    removed -t

--- fabfile.py
+++ fabfile.py
@@ -3,4 +3,4 @@ from fabric.api import *
 container_name="project1"
 @task
 def create_container():
-    local("docker run --name {} -t -i my-redhat-jenkins-slave supervisord".format(container_name))
+    local("docker run --name {}  -i my-redhat-jenkins-slave supervisord".format(container_name))
