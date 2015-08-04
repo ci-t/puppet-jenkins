d93707d0baee4a4956aff687387608e3dbb6109d
ci-t <roque@test.com>
2015-08-04 10:31:21 -0300

    added container name

--- fabfile.py
+++ fabfile.py
@@ -3,4 +3,4 @@ from fabric.api import *
 
 @task
 def create_container(name):
-    local("docker run --name {} -d -i my-redhat-jenkins-slave /bin/bash /etc/init.d/slave".format(name))
+    local("docker run --name {} -d -i my-redhat-jenkins-slave /bin/bash /etc/init.d/slave {}".format(name,name))
