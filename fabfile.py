be5ca42e1d62631a7bb9d62becbc2e8050aeb1b7
ci-t <roque@test.com>
2015-08-04 13:08:13 -0300

    label

--- fabfile.py
+++ fabfile.py
@@ -3,5 +3,5 @@ from fabric.api import *
 
 @task
 def create_container(name,project):
-    label=project-name
+    label=project
     local("docker run --name {} -d -i my-redhat-jenkins-slave /bin/bash /etc/init.d/slave {} {}".format(name,name,label))
